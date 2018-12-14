5bb78a692f82a4542a0a9b1c50d1a92d4b93778f
Ikabura <itsfree164@gmail.com>
2018-12-14 19:31:59 +0100

    Primer Checkpoint(14/12/18)

---
+++ Assets/Botones.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Botones : MonoBehaviour {
+	public Button Options, Play, Exit;
+
+	// Use this for initialization
+	void Start () {
+		Play.onClick.AddListener(play);
+		Options.onClick.AddListener(options);
+		Exit.onClick.AddListener(exit);
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void play () {
+
+	}
+	void options () {
+
+	}
+	void exit () {
+
+	}
+
+}
